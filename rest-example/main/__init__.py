c79409ef7c811de7ccb82f4559219f39a08b8165
Fernando Mattioli <mattioli.fernando@gmail.com>
2023-08-08 22:57:34 -0300

    Configure gunicorn

--- rest-example/main/__init__.py
+++ rest-example/main/__init__.py
@@ -4,16 +4,16 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 
-BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(os.path.join(BASE_DIR, '.env'))
 
-db = SQLAlchemy()
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URI")
 app.config["SQLALCHEMY_BINDS"] = {
-    "second": os.environ.get("SECOND_DATABASE_URI")
+    "second": os.environ.get("SECOND_DATABASE_URI"),
 }
-db.init_app(app)
+
+db = SQLAlchemy(app)
 
 import module_a.endpoints
 
